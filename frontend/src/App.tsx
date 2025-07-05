626d0b3633ae8f8535d4ebba10873f12b42ee96e
Arush Gupta <arush.agu@protonmail.com>
2025-07-05 19:31:33 +0530

    Update Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+        <h1 className="text-center text-4xl font-bold">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
